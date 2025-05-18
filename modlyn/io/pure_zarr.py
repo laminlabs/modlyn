7890645fa5e49e8522e61b1dfcafb1edc7aad860
Koncopd <koncopd@gmail.com>
2025-05-18 12:42:28 +0200

    renames

--- modlyn/io/pure_zarr.py
+++ modlyn/io/pure_zarr.py
@@ -23,12 +23,12 @@ class ZarrArraysDataset:
         self.preload_chunks = preload_nchunks
 
         self.n_obs_list: list[int] = []  # number of observations for each array
-        self.chunk_lengths: list[int] = []  # chunk length for each array
+        self.chunks_lengths: list[int] = []  # chunk length for each array
         arrays_chunks: list[list[int]] = []  # list of chunk indices for each array
         arrays_nchunks: list[int] = []  # number of chunks for each array
         for array in arrays:
             self.n_obs_list.append(array.shape[0])
-            self.chunk_lengths.append(array.chunk[0])
+            self.chunks_lengths.append(array.chunk[0])
             array_nchunks = array.nchunks
             arrays_nchunks.append(array_nchunks)
             arrays_chunks.append(np.arange(array_nchunks))
@@ -37,12 +37,12 @@ class ZarrArraysDataset:
         self.chunks = np.hstack(arrays_chunks)
         self.array_idxs = np.repeat(np.arange(len(self.arrays)), arrays_nchunks)
         # pre-compute chunk slices
-        self.chunk_slices: list[slice] = []
+        self.chunks_slices: list[slice] = []
         for i, chunk_idx in enumerate(self.chunks):
-            self.chunk_slices.append(self._chunk_slice(chunk_idx, self.array_idxs[i]))
+            self.chunks_slices.append(self._chunk_slice(chunk_idx, self.array_idxs[i]))
 
     def _chunk_slice(self, chunk_idx: int, array_idx: int):
-        chunk_length = self.chunk_lengths[array_idx]
+        chunk_length = self.chunks_lengths[array_idx]
         array_n_obs = self.n_obs_list[array_idx]
 
         start = chunk_length * chunk_idx
@@ -54,7 +54,7 @@ class ZarrArraysDataset:
         for i in chunk_idxs:
             array_idx = self.array_idxs[i]
             array = self.arrays[array_idx]
-            tasks.append(array._async_array.getitem(self.chunk_slices[i]))
+            tasks.append(array._async_array.getitem(self.chunks_slices[i]))
         if len(tasks) == 1:
             return tasks[0]
         else:
