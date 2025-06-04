9f6771129780fabde139aa0911409cd15202f74b
Koncopd <koncopd@gmail.com>
2025-06-04 16:33:46 +0200

    restore imports

--- modlyn/io/__init__.py
+++ modlyn/io/__init__.py
@@ -23,7 +23,7 @@ Array store creation.
    create_store_from_h5ads
 
 """
-# from .datamodules import ClassificationDataModule
-# from .loading import ZarrDataset, read_lazy
+from .datamodules import ClassificationDataModule
+from .loading import ZarrDataset, read_lazy
 from .pure_zarr import ZarrArraysDataset
 from .store_creation import create_store_from_h5ads
