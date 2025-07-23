e41508e1b7890764ecd716557ecccfcaf97726df
Alex Wolf <f.alexander.wolf@gmail.com>
2025-07-23 16:17:12 +0200

    👷 No need for auth

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,12 @@
 import shutil
 
 import lamindb_setup as ln_setup
-import pytest
 
 
 def pytest_sessionstart():
     ln_setup.init(storage="./testdb")
 
 
-def pytest_sessionfinish(session: pytest.Session):
+def pytest_sessionfinish():
     shutil.rmtree("./testdb")
     ln_setup.delete("testdb", force=True)
